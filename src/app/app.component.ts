e0469bdd9c9eefe4ced6e40cfcc1379ff4132e95
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2025-08-01 14:32:24 +0200

    feat(chat): refactor chat views with member components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MembersComponent } from '../members/members.component';
 import { MessageComponent } from '../message/message.component';
+import { ChatsComponent } from '../chats/chats.component';
+import { ChatComponent } from '../chat/chat.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [MembersComponent, MessageComponent],
+  imports: [ChatsComponent, ChatComponent],
 })
 export class AppComponent {
-  title = 'chat-interface';
+  title = 'chat-AI';
 }
